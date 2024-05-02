0388eed2c4a793aaec1f7db28294a8c866083c81
백진암 <jinam1234567@gmail.com>
2024-05-02 20:05:03 +0900

    CORS 설정

--- src/main/java/com/backend/domain/admin/presentation/AdminController.java
+++ src/main/java/com/backend/domain/admin/presentation/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/admin")
 public class AdminController {
 
